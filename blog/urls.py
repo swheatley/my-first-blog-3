ca26b4aa7233a890ea7cc5d34006a007e9dc60d4
swheatley <shaylee1@me.com>
2015-10-12 23:38:29 -0600

    Made sure code worked. Able to make and save posts.  Changed up CSS

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
@@ -7,6 +8,7 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
+
 
 
 
